d4deccbbba35dee48c94b95f859dba8b19110656
gusgus374 <gusalvarez374@gmail.com>
2025-03-07 16:51:45 -0500

    Update Emily's Streamlit page with new title and video

--- roster/emily.py
+++ roster/emily.py
@@ -1,21 +1,23 @@
+# write code below!
 import streamlit as st
-st.title('LALALA')
-if st.button('YES!'):
-    st.video('./media/emily_dribble.mp4')
+
+st.title('Me Win')
+if st.button('EMILY DE GREAT turns on the JETS'):
+    st.video('./media/emily_goal.mp4')
 if st.button('SOCCER'):
     st.toast(":soccer:")
-if st.button("jeufyfud"):
-    st.ballons()
+
 with st.expander("Show Emily's code"):
     st.code(
         body='''
-st.title('LALALA')
-if st.button('YES!'):
-    st.video('./media/emily_dribble.mp4')
+# write code below!
+import streamlit as st
+
+st.title('Me Win')
+if st.button('EMILY DE GREAT turns on the JETS'):
+    st.video('./media/emily_goal.mp4')
 if st.button('SOCCER'):
     st.toast(":soccer:")
-if st.button("jeufyfud"):
-    st.ballons()
         ''',
         language="python",
         line_numbers=True
